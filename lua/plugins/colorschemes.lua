7455c8c0ad2f66a5ce2adb625ce99e59e8923f55
mcreinii <mcreinii@gmail.com>
2025-10-23 10:48:39 +0000

    Added colorschemes
    YES

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,35 @@
+-- Set colorscheme at core/ui.lua
+-- Insert your themes here, I suggest putting more than one for quick switching.
+return {
+	{
+		"craftzdog/solarized-osaka.nvim",
+		priority = 1000,
+		lazy = false,
+	},
+	{
+		"bluz71/vim-moonfly-colors",
+		priority = 1000,
+		lazy = false,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = { theme = "dragon" },
+	},
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		lazy = false,
+	},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		lazy = false,
+	},
+	{
+	"dasupradyumna/midnight.nvim",
+		priority = 1000,
+		lazy = false,
+	},
+}
