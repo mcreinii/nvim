0a0bf6a692c9258a41975154c9ae550497174680
mcreinii <mcreinii@gmail.com>
2025-10-23 08:25:44 +0000

    Initial commit, I forgot.
    Neo-tree
    Lualine
    Tree-sitter

---
+++ lua/plugins/core.lua
@@ -0,0 +1,6 @@
+return {
+	-- Global dependencies, most plugins needs these.
+	"nvim-lua/plenary.nvim", -- Utility stuff
+	"nvim-tree/nvim-web-devicons", -- Pretty icons
+	"MunifTanjim/nui.nvim", -- UI components
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,107 @@
+return {
+	-- Neo-Tree file explorer
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		},
+		config = function()
+			-- Toggle using <leader>ne
+			vim.keymap.set(
+				"n",
+				"<leader>ne",
+				"<cmd>Neotree toggle<cr>",
+				{ noremap = true, silent = true, desc = "Toggle file tree/explorer (Neotree)" }
+			)
+		end,
+		lazy = false,
+	},
+
+	-- Nvim-Treesitter for syntax highlighting.
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		lazy = false,
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = { "lua", "vim" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+
+				-- You can add your own highlight disable logic here.
+
+				additional_vim_regex_highlighting = false,
+			},
+		},
+	},
+
+	-- Status line (Lualine)
+	-- Info for your mode, branch language, LSP and etc.
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				always_show_tabline = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+					refresh_time = 16, -- ~60fps
+					events = {
+						"WinEnter",
+						"BufEnter",
+						"BufWritePost",
+						"SessionLoadPost",
+						"FileChangedShellPost",
+						"VimResized",
+						"Filetype",
+						"CursorMoved",
+						"CursorMovedI",
+						"ModeChanged",
+					},
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "filetype", "diff", "diagnostics" },
+				lualine_c = {},
+				lualine_x = { "encoding", "fileformat" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
+		},
+	},
+}
