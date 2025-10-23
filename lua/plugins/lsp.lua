f79d59fa0fb56abfdf191761df86afd469c900c0
mcreinii <mcreinii@gmail.com>
2025-10-23 17:01:01 +0000

    Small change
    Only shows lsp keybinds if lsp is available

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,74 +10,80 @@ return {
 			{
 				"neovim/nvim-lspconfig",
 				config = function()
-					-- Code action
-					vim.keymap.set(
-						"n",
-						"<leader>la",
-						vim.lsp.buf.code_action,
-						{ noremap = true, silent = true, desc = "Code action (LSP)" }
-					)
+					vim.api.nvim_create_autocmd("LspAttach", {
+						callback = function(args)
+              local bufnr = args.buf
 
-					-- Source action
-					vim.keymap.set("n", "<leader>lA", function()
-						vim.lsp.buf.code_action({
-							context = { only = { "source" } },
-							apply = false,
-						})
-					end, { noremap = true, silent = true, desc = "Source action (LSP)" })
+							-- Code action
+							vim.keymap.set(
+								"n",
+								"<leader>la",
+								vim.lsp.buf.code_action,
+								{ noremap = true, silent = true, buffer = bufnr, desc = "Code action (LSP)" }
+							)
 
-					-- Diagnostics
-					vim.keymap.set("n", "<leader>[d", function()
-						vim.diagnostic.jump({ count = -1, float = true })
-					end, { noremap = true, silent = true, desc = "Diagnostic (Prev)" })
-					vim.keymap.set("n", "<leader>]d", function()
-						vim.diagnostic.jump({ count = 1, float = true })
-					end, { noremap = true, silent = true, desc = "Diagnostic (Prev)" })
+							-- Source action
+							vim.keymap.set("n", "<leader>lA", function()
+								vim.lsp.buf.code_action({
+									context = { only = { "source" } },
+									apply = false,
+								})
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Source action (LSP)" })
 
-					-- Format
-					vim.keymap.set("n", "<leader>lf", function()
-						vim.lsp.buf.format()
-					end, { noremap = true, silent = true, desc = "Format buffer (LSP)" })
+							-- Diagnostics
+							vim.keymap.set("n", "<leader>[d", function()
+								vim.diagnostic.jump({ count = -1, float = true })
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Diagnostic (Prev)" })
+							vim.keymap.set("n", "<leader>]d", function()
+								vim.diagnostic.jump({ count = 1, float = true })
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Diagnostic (Prev)" })
 
-					-- Rename
-					vim.keymap.set("n", "<leader>lr", function()
-						vim.lsp.buf.rename()
-					end, { noremap = true, silent = true, desc = "Rename symbol (LSP)" })
+							-- Format
+							vim.keymap.set("n", "<leader>lf", function()
+								vim.lsp.buf.format()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Format buffer (LSP)" })
 
-					-- Hover
-					vim.keymap.set("n", "<leader>lr", function()
-						vim.lsp.buf.hover()
-					end, { noremap = true, silent = true, desc = "Hover (LSP)" })
+							-- Rename
+							vim.keymap.set("n", "<leader>lr", function()
+								vim.lsp.buf.rename()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Rename symbol (LSP)" })
 
-					-- Signatures
-					vim.keymap.set("n", "<leader>ls", function()
-						vim.lsp.buf.signature_help()
-					end, { noremap = true, silent = true, desc = "Signatures (LSP)" })
+							-- Hover
+							vim.keymap.set("n", "<leader>lr", function()
+								vim.lsp.buf.hover()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Hover (LSP)" })
 
-					-- Go to Definitions
-					vim.keymap.set("n", "<leader>lgd", function()
-						vim.lsp.buf.definition()
-					end, { noremap = true, silent = true, desc = "Go to Definitions (LSP)" })
+							-- Signatures
+							vim.keymap.set("n", "<leader>ls", function()
+								vim.lsp.buf.signature_help()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Signatures (LSP)" })
 
-					-- Go to References
-					vim.keymap.set("n", "<leader>lgr", function()
-						vim.lsp.buf.hover()
-					end, { noremap = true, silent = true, desc = "Go to References (LSP)" })
+							-- Go to Definitions
+							vim.keymap.set("n", "<leader>lgd", function()
+								vim.lsp.buf.definition()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Go to Definitions (LSP)" })
 
-					-- Go to Declarations
-					vim.keymap.set("n", "<leader>lgD", function()
-						vim.lsp.buf.declaration()
-					end, { noremap = true, silent = true, desc = "Hover (LSP)" })
+							-- Go to References
+							vim.keymap.set("n", "<leader>lgr", function()
+								vim.lsp.buf.hover()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Go to References (LSP)" })
 
-					-- Go to Implementation
-					vim.keymap.set("n", "<leader>lgi", function()
-						vim.lsp.buf.implementation()
-					end, { noremap = true, silent = true, desc = "Go to Implementation (LSP)" })
+							-- Go to Declarations
+							vim.keymap.set("n", "<leader>lgD", function()
+								vim.lsp.buf.declaration()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Hover (LSP)" })
 
-					-- Types
-					vim.keymap.set("n", "<leader>lt", function()
-						vim.lsp.buf.type_definition()
-					end, { noremap = true, silent = true, desc = "Type Definition (LSP)" })
+							-- Go to Implementation
+							vim.keymap.set("n", "<leader>lgi", function()
+								vim.lsp.buf.implementation()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Go to Implementation (LSP)" })
+
+							-- Types
+							vim.keymap.set("n", "<leader>lt", function()
+								vim.lsp.buf.type_definition()
+							end, { noremap = true, silent = true, buffer = bufnr, desc = "Type Definition (LSP)" })
+						end,
+					})
 				end,
 			},
 		},
