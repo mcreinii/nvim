dfba5b5a0c02f53548c07c0fb7304ca1c69634b9
mcreinii <mcreinii@gmail.com>
2025-10-26 23:00:22 +0800

    Moonfly and snacks.nvim
    cool ass dashboard.

---
+++ lua/plugins/ui/snacks.lua
@@ -0,0 +1,55 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  ---@class snacks.dashboard.Config
+  ---@field enabled? boolean
+  ---@field sections snacks.dashboard.Section
+  ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
+  opts = {
+    dashboard = {
+      preset = {
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          {
+            icon = " ",
+            key = "f",
+            desc = "Find File",
+            action = ":lua Snacks.dashboard.pick('files')"
+          },
+          {
+            icon = "󰒲 ",
+            key = "L",
+            desc = "Lazy",
+            action = ":Lazy",
+            enabled = package.loaded.lazy ~= nil
+          },
+          {
+            icon = " ",
+            key = "q",
+            desc = "Quit",
+            action = ":qa"
+          }
+        },
+        header = [[
+▖  ▖  ▖ ▖  ▘   
+▛▖▞▌▛▘▛▖▌▌▌▌▛▛▌
+▌▝ ▌▙▖▌▝▌▚▘▌▌▌▌
+        ]],
+      },
+      sections = {
+        {
+          section = "keys"
+        },
+        {
+          pane = 2,
+          section = "header",
+        },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "pipe.sh || bash-pipes"
+        }
+      }
+    },
+  },
+}
