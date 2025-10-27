f5136a0c5c810f5cc5751eb6fac5a0136ff83e0d
mcreinii <mcreinii@gmail.com>
2025-10-27 02:25:20 +0000

    Bar :3

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -14,15 +14,41 @@ return {
 			lualine_c = {},
 			lualine_x = { "lsp_status" },
 			lualine_y = { "location" },
-			lualine_z = { "progress" },
-		},
-		inactive_sections = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = {},
-		},
-	},
+			lualine_z = {
+				{
+					function()
+						local current_line = vim.fn.line(".")
+						local total_lines = vim.fn.line("$")
+						local char_empty = " "
+						local char_bar = "█"
+				  local size = 3
+
+				  local line_ratio = current_line / total_lines
+
+				  local index = math.ceil(line_ratio * size)
+
+				  local bar = ""
+
+				  for len = 0, size - 1 do
+					  if len < index then
+						  bar = bar .. char_empty
+					  else
+						  bar = bar .. char_bar
+					  end
+				  end
+
+				  return bar
+			  end,
+			  padding = { left = 0, right = 0 },
+		  },
+	  },
+  },
+  inactive_sections = {
+	  lualine_a = {},
+	  lualine_b = {},
+	  lualine_c = {},
+	  lualine_x = {},
+	  lualine_y = {},
+	  lualine_z = {},
+  },  },
 }
