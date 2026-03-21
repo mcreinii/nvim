42519e6d17f4cc34b701df3bb7d093064a93e383
mcreinii <mcreinii@gmail.com>
2026-03-21 18:26:39 +0000

    Main file explorer (telescope)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,14 +3,30 @@ return {
     version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-file-browser.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function()
-        local builtin = require('telescope.builtin')
+    opts = {
+        extensions = {
+            file_browser = {
+                hijack_netrw = true,
+            },
+        },
+    },
+    config = function(_, opts)
+        require('telescope').setup(opts)
+        require('telescope').load_extension('file_browser')
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files)
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep)
-        vim.keymap.set('n', '<leader>fb', builtin.buffers)
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+        local keymap = vim.keymap.set
+        local builtin = require('telescope.builtin')
+        local extensions = require('telescope').extensions
+
+        keymap('n', '<leader>ff', builtin.find_files)
+        keymap('n', '<leader>fr', builtin.oldfiles)
+        keymap('n', '<leader>fg', builtin.live_grep)
+        keymap('n', '<leader>fb', builtin.buffers)
+        keymap('n', '<leader>fd', builtin.diagnostics)
+        keymap('n', '<leader>e', extensions.file_browser.file_browser)
+        keymap('n', '<leader>fh', builtin.help_tags)
     end,
 }
