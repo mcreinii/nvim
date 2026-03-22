e9e6ba998fd9ca73eaed721feb0f11dcbf6d150a
mcreinii <mcreinii@gmail.com>
2026-03-22 01:35:57 +0000

    Jellybeans high contrast theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('github-theme').setup({})
-
-        vim.cmd('colorscheme github_dark_high_contrast')
-    end,
+    {
+        'wtfox/jellybeans.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd('colorscheme jellybeans-hc')
+        end,
+    },
 }
