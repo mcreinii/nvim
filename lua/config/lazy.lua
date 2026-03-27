511b6055c0a446381fd8dc845872d462a9b0ee8b
mcreinii <mcreinii@gmail.com>
2026-03-27 07:23:39 +0000

    Lazy update checker is annoying

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,6 +27,5 @@ require('lazy').setup({
     spec = {
         { import = 'plugins' },
     },
-    install = { colorscheme = { 'habamax' } },
-    checker = { enabled = true },
+    checker = { enabled = false },
 })
