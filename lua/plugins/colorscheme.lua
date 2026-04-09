478c5f81eca644f3dfd379d99ca337cfb3032c31
mcreinii <mcreinii@gmail.com>
2026-04-09 17:49:23 +0000

    Catppuccin transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,7 +8,16 @@ return {
     --         vim.cmd('colorscheme jellybeans-hc')
     --     end,
     -- },
-    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, config = function ()
-        vim.cmd('colorscheme catppuccin-mocha')
-    end },
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        priority = 1000,
+        opts = {
+            transparent_background = true,
+        },
+        config = function(_, opts)
+            require('catppuccin').setup(opts)
+            vim.cmd('colorscheme catppuccin-mocha')
+        end,
+    },
 }
